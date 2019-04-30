afda91dfa3ae033ce77b8e56388ddf4218669a47
heera88 <rdmmastermind@gmail.com>
2019-04-30 01:06:23 -0400

    use spring cloud config server

--- src/main/java/com/rdmstudios/scsnetflix/controllers/FortuneController.java
+++ src/main/java/com/rdmstudios/scsnetflix/controllers/FortuneController.java
@@ -3,6 +3,7 @@ package com.rdmstudios.scsnetflix.controllers;
 import com.rdmstudios.scsnetflix.domain.Fortune;
 import com.rdmstudios.scsnetflix.repositories.FortuneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,9 @@ import java.util.Optional;
 
 @RestController
 public class FortuneController {
+
+    @Value("${fortune:Eat a fortune cookie!}")
+    private String fortune;
 
     @Autowired
     FortuneRepository fortuneRepository;
@@ -23,5 +27,10 @@ public class FortuneController {
     @GetMapping("/fortunes/{id}")
     public Optional<Fortune> fortuneById(@PathVariable Long id) {
         return fortuneRepository.findById(id);
+    }
+
+    @GetMapping("/")
+    public String todaysFortune() {
+        return fortune;
     }
 }
