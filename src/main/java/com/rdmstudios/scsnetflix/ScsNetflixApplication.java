f8ee3ce41da37a214184b262f051c42f452fd21c
heera88 <rdmmastermind@gmail.com>
2019-05-02 15:37:27 -0400

    http authorize requests: permit all

--- src/main/java/com/rdmstudios/scsnetflix/ScsNetflixApplication.java
+++ src/main/java/com/rdmstudios/scsnetflix/ScsNetflixApplication.java
@@ -2,12 +2,26 @@ package com.rdmstudios.scsnetflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ScsNetflixApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScsNetflixApplication.class, args);
 	}
 
+	@Configuration
+	protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll();
+		}
+
+	}
 }
--- src/main/java/com/rdmstudios/scsnetflix/repositories/FortuneRepository.java
+++ src/main/java/com/rdmstudios/scsnetflix/repositories/FortuneRepository.java
@@ -2,6 +2,7 @@ package com.rdmstudios.scsnetflix.repositories;
 
 import com.rdmstudios.scsnetflix.domain.Fortune;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
 public interface FortuneRepository extends CrudRepository<Fortune, Long> {
